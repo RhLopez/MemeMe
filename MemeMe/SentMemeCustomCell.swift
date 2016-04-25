3cf24fc503ea3ecda5f05aa3b6a52ec5d28a95a0
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2016-04-24 22:35:58 -0700

    Add collectionView scene

---
+++ MemeMe/SentMemeCustomCell.swift
@@ -0,0 +1,16 @@
+//
+//  SentMemeCustomCell.swift
+//  MemeMe
+//
+//  Created by Ramiro H. Lopez on 4/23/16.
+//  Copyright © 2016 Ramiro H. Lopez. All rights reserved.
+//
+
+import UIKit
+
+class SentMemeCustomCell: UITableViewCell {
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var memeLabel: UILabel!
+    
+}
--- MemeMe/SentMemesTableViewController.swift
+++ MemeMe/SentMemesTableViewController.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class SentMemesTableViewController: UITableViewController {
     
+    override func viewDidAppear(animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    var memes: [Meme] {
+        return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
+    }
+    
+    // MARK: -UITableViewController
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("sentMemeCell") as! SentMemeCustomCell
+        let meme = memes[indexPath.row]
+        
+        cell.memeImageView.image = meme.memedImage
+        cell.memeLabel.text = meme.topText + " " + meme.bottomText
+    
+        return cell
+    }
 }
